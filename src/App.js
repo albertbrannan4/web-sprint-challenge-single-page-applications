98edf38e007cb944f9810075b14d879b23471868
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-01-13 21:13:34 -0500

    rending inputs on the screen next to up connect state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import Home from './Home';
+import Form from "./Form";
 
+const menu = {
+  size: [
+    { value: 'small', label: 'Small' },
+    { value: 'medium', label: 'Medium' },
+    { value: 'large', label: 'Large' },
+    { value: 'xlarge', label: 'X-Large' }
+  ],
+  sauces: [
+    { value: 'orginalRed', label: 'Orginal Red',name:'sauce' },
+    { value: 'garlicRanch', label: 'Garlic Ranch',name:'sauce' },
+    { value: 'bbqSauce', label: 'BBQ Sauce',name:'sauce' },
+    { value: 'spinachAlfredo', label: 'Spinach Alfredo',name:'sauce' },
+  ],
+  toppings:[
+    {value:'pepperoni',label:'Pepperoni'},
+    {value:'sausage',label:'Sausage'},
+    {value:'canadianBacon',label:'Canadian Bacon'},
+    {value:'spicyItalianSausage',label:'Spicy Italian Sausage'},
+    {value:'grilledChicken',label:'Grilled Chicken'},
+    {value:'onion',label:'Onion'},
+    {value:'greenPepper',label:'Green Pepper'},
+    {value:'dicedTomatos',label:'Diced Tomatos'},
+    {value:'blackOlives',label:'Black Olives'},
+    {value:'roastedGarlic',label:'Roasted Garlic'},
+    {value:'artichokeHearts',label:'Artichoke Hearts'},
+    {value:'threeCheese',label:'Three Cheese'},
+    {value:'pineapple',label:'Pineapple'},
+    {value:'extraCheese',label:'Extra Cheese'},
+  ]
+}
 const App = () => {
+  const [pizzaMenu, setMenu] = useState(menu);
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <nav>
+        <Link  to='/'>Home</Link>
+        <Link id="order-pizza" to='/pizza'>Order</Link>
+      </nav>
+      <Switch>
+        <Route path='/pizza'>
+          <Form menu={pizzaMenu} />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
     </>
   );
 };
---
+++ src/Form.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+const Form = (props) => {
+    const { menu } = props
+    const onChange = (e) => {
+        const { name, value } = e.target;
+        console.log(name, " ", value);
+    }
+    const onSubmit = (e)=>{
+        e.preventDefault();
+    }
+    return (
+        <div>
+            <div><h2>Build Your Own Pizza</h2></div>
+            <div>
+                <img src="https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="pizza" />
+            </div>
+            <div>
+                <h3>Build Your Own Pizza</h3>
+                <form onSubmit={onSubmit} id='pizza-form'>
+                   <div>
+                    <label>Name
+                    <input id='name-input' type='text' name='name' />
+                    </label>
+                   </div>
+                    <div>
+                        <h4>Choice of Size</h4>
+                        <p>Required</p>
+                    </div>
+                    <div>
+                        <select onChange={onChange} name='size' id='size-dropdown'>
+                            {menu.size.map((size, idx) => {
+                                return (<option key={idx} value={size.value}  >{size.label}</option>)
+                            })}
+                        </select>
+                    </div>
+                    <div>
+                        <h4>Choice of Sauce</h4>
+                        <p>Required</p>
+                    </div>
+                    <div>
+                        {menu.sauces.map((sauce, idx) => {
+                            return (
+                                <div key={idx}>
+                                    <input onChange={onChange} type='radio' value={sauce.value} name={sauce.name} />{sauce.label}
+                                </div>)
+                        })}
+                    </div>
+                    <div>
+                        <h4>Add Toppings</h4>
+                        <p>Choose up to 10</p>
+                    </div>
+                    <div>
+                        {menu.toppings.map((topping, idx) => {
+                            return (
+                                <label key={idx}>
+                                    <input type='checkbox' onChange={onChange} value={topping.value} name={`topping`} />{topping.label}
+                                </label>
+                            )
+                        })}
+                    </div>
+                    <input type='submit' value='Submit Pizza'/>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Form;
