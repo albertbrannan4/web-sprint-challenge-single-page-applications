384db26ad07aa47e73d9f62625bb4313e37a1fb0
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-01-13 22:34:02 -0500

    updated initial state

--- src/App.js
+++ src/App.js
@@ -12,49 +12,61 @@ const menu = {
     { value: 'xlarge', label: 'X-Large' }
   ],
   sauces: [
-    { value: 'orginalRed', label: 'Orginal Red',name:'sauce' },
-    { value: 'garlicRanch', label: 'Garlic Ranch',name:'sauce' },
-    { value: 'bbqSauce', label: 'BBQ Sauce',name:'sauce' },
-    { value: 'spinachAlfredo', label: 'Spinach Alfredo',name:'sauce' },
+    { value: 'orginalRed', label: 'Orginal Red', name: 'sauce' },
+    { value: 'garlicRanch', label: 'Garlic Ranch', name: 'sauce' },
+    { value: 'bbqSauce', label: 'BBQ Sauce', name: 'sauce' },
+    { value: 'spinachAlfredo', label: 'Spinach Alfredo', name: 'sauce' },
   ],
-  toppings:[
-    {value:'pepperoni',label:'Pepperoni'},
-    {value:'sausage',label:'Sausage'},
-    {value:'canadianBacon',label:'Canadian Bacon'},
-    {value:'spicyItalianSausage',label:'Spicy Italian Sausage'},
-    {value:'grilledChicken',label:'Grilled Chicken'},
-    {value:'onion',label:'Onion'},
-    {value:'greenPepper',label:'Green Pepper'},
-    {value:'dicedTomatos',label:'Diced Tomatos'},
-    {value:'blackOlives',label:'Black Olives'},
-    {value:'roastedGarlic',label:'Roasted Garlic'},
-    {value:'artichokeHearts',label:'Artichoke Hearts'},
-    {value:'threeCheese',label:'Three Cheese'},
-    {value:'pineapple',label:'Pineapple'},
-    {value:'extraCheese',label:'Extra Cheese'},
+  toppings: [
+    { value: 'pepperoni', label: 'Pepperoni' },
+    { value: 'sausage', label: 'Sausage' },
+    { value: 'canadianBacon', label: 'Canadian Bacon' },
+    { value: 'spicyItalianSausage', label: 'Spicy Italian Sausage' },
+    { value: 'grilledChicken', label: 'Grilled Chicken' },
+    { value: 'onion', label: 'Onion' },
+    { value: 'greenPepper', label: 'Green Pepper' },
+    { value: 'dicedTomatos', label: 'Diced Tomatos' },
+    { value: 'blackOlives', label: 'Black Olives' },
+    { value: 'roastedGarlic', label: 'Roasted Garlic' },
+    { value: 'artichokeHearts', label: 'Artichoke Hearts' },
+    { value: 'threeCheese', label: 'Three Cheese' },
+    { value: 'pineapple', label: 'Pineapple' },
+    { value: 'extraCheese', label: 'Extra Cheese' },
   ]
 }
-const initialFormValues={
-  name:'',
-  size:'',
-  sauce:'',
-  toppings:'',
-  glutenFree:false,
-  specialInstructions:''
+
+const initialFormValues = {
+  name: '',
+  size: '',
+  sauce: '',
+  sausage: false,
+  canadianBacon: false,
+  spicyItalianSausage: false,
+  grilledChicken: false,
+  onion: false,
+  greenPepper: false,
+  dicedTomatos: false,
+  blackOlives: false,
+  roastedGarlic: false,
+  artichokeHearts: false,
+  threeCheese: false,
+  extraCheese: false,
+  glutenFree: false,
+  specialInstructions: ''
 }
 const App = () => {
   const [pizzaMenu, setMenu] = useState(menu);
   const [formValues, setFormValues] = useState(initialFormValues);
 
-  const formHandler = (name,value)=>{
-    setFormValues({...formValues,[name]:value})
+  const formHandler = (name, value) => {
+    setFormValues({ ...formValues, [name]: value })
   }
   console.log(formValues);
   return (
     <>
       <h1>Lambda Eats</h1>
       <nav>
-        <Link  to='/'>Home</Link>
+        <Link to='/'>Home</Link>
         <Link id="order-pizza" to='/pizza'>Order</Link>
       </nav>
       <Switch>
